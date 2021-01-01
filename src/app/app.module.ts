d90eec38dbee0e30387563242e55fb3853e75bce
rajnish21a <in.rajnish@live.in>
2021-01-02 00:59:02 +0530

    Login for with Reactive Form Style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { PastTrainingComponent } from './training/past-training/past-training.co
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
@@ -34,7 +34,8 @@ import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
     AppRoutingModule,
     FlexLayoutModule,
     FormsModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    ReactiveFormsModule
   ],
   providers: [{
     provide: MAT_DATE_LOCALE, useValue: 'en-GB'
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  public maxDate!: Date;
+  public loginForm!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+    this.loginForm = new FormGroup({
+      'email': new FormControl('',[Validators.required, Validators.email] ),
+      'password': new FormControl('',Validators.required)
+    });
+  }
+
+  public onSubmit(): void {
+    console.log(this.loginForm.value)
   }
 
 }
